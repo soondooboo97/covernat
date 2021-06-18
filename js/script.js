6780fdfe63d099eeb2a8a5f9d9c8dab889be767a
soondooboo97 <82435741+soondooboo97@users.noreply.github.com>
2021-06-18 17:04:47 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,7 +9,23 @@
      },
  });
 
+ //------------스와이퍼 슬라이드 반응형-------------
 
+var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1, //640~1024 해상도 외 레이아웃 뷰 개수
+      spaceBetween: 10, //위 slidesPerview 여백
+      breakpoints: { //반응형 조건 속성
+        640: { //640 이상일 경우
+          slidesPerView: 2, //레이아웃 2열
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+      }
+});
  //------------롤 이미지-------------
 
  var rollLeft = 0;
